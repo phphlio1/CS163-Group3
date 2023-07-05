961ce9a04a13c978d57e2d4ab5e0164dc03f684c
phphlio-1 <phamphuloi01@gmail.com>
2023-07-05 13:36:46 +0700

    Read Data Into Trie (From English_English_original) complete

---
+++ Dictionary/Trie.cpp
@@ -0,0 +1,85 @@
+#include "Trie.h"
+int convertCharToNum(char c)
+{
+    if('0' <= c && c <= '9')
+        return c - '0';
+    else if('!' <= c && c <= '/')
+        return c - '!' + 10;
+    else if(':' <= c && c <= '@')
+        return c - ':' + 25;
+    else if('[' <= c && c <= '`')
+        return c - '[' + 32;
+    else if('{' <= c && c <= '~')
+        return c - '{' + 38;
+    else if(c == 32) // Space character
+        return 42;
+    else if('a' <= c && c <= 'z')
+        return c - 'a' + 43;
+    else if('A' <= c && c <= 'Z')
+        return c - 'A' + 43;
+    return -1;
+}
+
+void build_Trie_EngEng(TrieNode*& root)
+{
+	std::ifstream fin;
+	fin.open("../data_set/English_English_original.txt");
+    while(fin)
+    {
+        std::string word;
+        getline(fin, word, (char)9); // (char)9 indicate 'TAB'
+        addWordAndDefiToTrie(word, root, fin);
+    }
+    fin.close();
+}
+
+void addWordAndDefiToTrie(std::string word, TrieNode*& root, std::ifstream& fin)
+{
+    
+	TrieNode* tmp = root;
+	for (int i = 0; i < word.size(); ++i)
+	{
+        int index = convertCharToNum(word[i]);
+		if (!root->edges[index])
+			root->edges[index] = new TrieNode;
+		root = root->edges[index];
+	}
+	root->isEndOfWord = true;
+    getline(fin, root->definition, '\n');
+	root = tmp;
+}
+
+bool findWordInTrie(std::string word, TrieNode* root)
+{
+	for (int i = 0; i < word.size(); ++i)
+	{
+        int index = convertCharToNum(word[i]);
+		if (!root->edges[index])
+			return false;
+		root = root->edges[index];
+	}
+	if (root->isEndOfWord)
+		return true;
+	else
+		return false;
+}
+
+void print(std::string ans, TrieNode* root)
+{
+	if (!findWordInTrie(ans, root))
+		std::cout << ans << " NOT FOUND!\n";
+	else
+		std::cout << ans << " FOUND!\n";
+}
+
+int main()
+{
+	TrieNode* root = new TrieNode;
+    build_Trie_EngEng(root);
+    print("'em", root);
+    print("'gainst", root);
+	print("apples", root);
+    print("mastax", root);
+	print("abc", root);
+	return 0;
+}
---
+++ Dictionary/Trie.h
@@ -0,0 +1,24 @@
+#ifndef TRIE_H
+#define TRIE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+struct TrieNode
+{
+    TrieNode* edges[69];
+    bool isEndOfWord;
+    std::string definition;
+
+    TrieNode()
+    {
+        for (int i = 0 ; i < 69; ++i) edges[i] = nullptr;
+        isEndOfWord = false;
+    }
+};
+int convertCharToNum(char c); 
+void build_Trie_EngEng(TrieNode*& root);
+
+void addWordAndDefiToTrie(std::string word, TrieNode*& root, std::ifstream& fin);
+bool findWordInTrie(std::string word, TrieNode* root);
+#endif
